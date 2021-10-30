7b4124801e6cf7257b5c6cb37a4a549e3543c981
Olavo <olavo.maximino@gmail.com>
2021-10-30 10:53:58 -0300

    rota post de usuários agora adiciona usuários ao banco

--- SRC/Routes/users.js
+++ SRC/Routes/users.js
@@ -9,13 +9,20 @@ router.get('/', async function (req, res){
     return res.send(users)
 })
 
-router.post('/', function (req, res){
+router.post('/', async function (req, res){
     //verificação dos campos da requisição
       if (!req.body || !req.body.nome || !req.body.sobrenome || !req.body.email || !req.body.telefone || !req.body.celular || !req.body.senha){
           console.log(400)
           return res.sendStatus(400)
       }
-      res.send(' ')
+      await usermodel.create({
+          name: req.body.nome,
+          lastName: req.body.sobrenome,
+          email: req.body.email,
+          phone: req.body.telefone,
+          mobile: req.body.celular
+      })
+      return res.sendStatus(201)
   })
 
 module.exports = router
